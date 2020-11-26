14243c40eb77f159df167f9bebab94edb221009e
Xwin <47206910+Xwin28@users.noreply.github.com>
2020-11-26 21:37:03 +0700

    Update Fixed Conflict

--- WebsiteDayNauAn/WebsiteDayNauAn/Pages/DangNhap_Pages.aspx
+++ WebsiteDayNauAn/WebsiteDayNauAn/Pages/DangNhap_Pages.aspx
@@ -4,25 +4,14 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title>Login</title>
-    <link href="../lib/css/StyleDN.css" rel="stylesheet" />
+    <title></title>
 </head>
 <body>
-    <form id="frm" runat="server">
-    <div class="login-box">
-  <h1>Login</h1>
-  <div class="textbox">
-     <asp:Image ID="Image2" CssClass="fas fa-user" runat="server" />
-    <asp:TextBox ID="txt_UserName" placeholder="Username" runat="server"></asp:TextBox>
-  </div>
-
-  <div class="textbox">
-    
-      <asp:Image ID="Image1" CssClass="fas fa-lock" runat="server" />
-      <asp:TextBox ID="txt_Password"  placeholder="Password" runat="server" TextMode="Password"></asp:TextBox>
-  </div>
-  <asp:Button ID="Btn_DangNhap" runat="server" Text="Sign in" CssClass="btn" OnClick="Btn_DangNhap_Click"  />
-</div>
-        </form>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txt_Password" runat="server"></asp:TextBox>
+            <asp:Button ID="Btn_DangNhap" runat="server" Text="Đăng Nhập" OnClick="Btn_DangNhap_Click" />
+        </div>
+    </form>
 </body>
 </html>
--- WebsiteDayNauAn/WebsiteDayNauAn/Pages/DangNhap_Pages.aspx.designer.cs
+++ WebsiteDayNauAn/WebsiteDayNauAn/Pages/DangNhap_Pages.aspx.designer.cs
@@ -15,40 +15,13 @@ namespace WebsiteDayNauAn.Pages
     {
 
         /// <summary>
-        /// frm control.
+        /// form1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm frm;
-
-        /// <summary>
-        /// Image2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image2;
-
-        /// <summary>
-        /// txt_UserName control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txt_UserName;
-
-        /// <summary>
-        /// Image1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Image Image1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
         /// txt_Password control.
